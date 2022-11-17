997c3c79a53879f3f25bc4c71e27a1189c2bb325
kenbme <guilhermekenned1@gmail.com>
2022-11-17 18:15:50 -0300

    Definindo rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Index from "./pages/Index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />}></Route>
+        <Route path="/cadastro" element={"CADASTRO"}></Route>
+        <Route path="/login" element={"LOGIN"}></Route>
+        <Route path="/users/editarUsuario" element={"EDITAR USUÁRIO"}></Route>
+        <Route path="/tarefas" element={"TAREFAS"}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Index.js
@@ -0,0 +1,33 @@
+function Index() {
+  return (
+    <>
+      <main>
+        <div id="apresentacao">
+          <h1>Codex - Project</h1>
+          <hr color="#13678A" />
+          <p class="paragrafo">
+            {" "}
+            Esse projeto tem por objetivo mostar a interatividade entre o
+            front-end e o back-end através de uma API. Com isso, criamos um
+            sistema na qual o usuário consegue se logar, cadastrar-se e ainda
+            fazer alterações na sua conta, uma delas é deletar à própria. Além
+            de ser um To-do List, o usúario ele consegue adicionar, remover ou
+            editar as suas tarefas do dia.
+          </p>
+          <div id="div-imagem">
+            <img
+              src="/images/api.png"
+              id="imagem-apresentação"
+              width="300px"
+              height="300px"
+              border="5px"
+              class="imagem-apresentacao"
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Index;
