10d7fa457e34119cbe4d554b421658a5992ca88a
kenbme <guilhermekenned1@gmail.com>
2022-11-20 15:51:04 -0300

    Delete task fetch

--- src/components/tarefa/Tarefa.js
+++ src/components/tarefa/Tarefa.js
@@ -28,67 +28,97 @@ export default function Task({ user, task }) {
   var _date = new Date(task.date);
   var defaultDate =
     _date.getFullYear() + "-" + (_date.getMonth() + 1) + "-" + _date.getDate();
-  
+
   function editTask() {
     var div_edit_tarefas = document.getElementById("edit-tarefas");
-      if (div_edit_tarefas.style.display === "none") {
-        div_edit_tarefas.style.display = "flex";
-      } else {
-        div_edit_tarefas.style.display = "none";
-      }
+    if (div_edit_tarefas.style.display === "none") {
+      div_edit_tarefas.style.display = "flex";
+    } else {
+      div_edit_tarefas.style.display = "none";
+    }
+  }
+
+  const deletaTask = () => {
+    fetch(`http://localhost:4000/users/${user.username}/tasks/${task._id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+        charset: "UTF-8",
+        "x-access-token": user.token,
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        if (!json.errors) {
+          window.alert("Recarregue as tarefas para visualizar as alterações.");
+        } else {
+          window.alert("ERRO: " + JSON.stringify(json.errors));
+        }
+      })
+      .catch((error) => console.log(error));
   };
 
-  return ( <>
+  return (
+    <>
+      <div id="main-tarefas">
+        <h1 className="topico">Tarefas</h1>
+        <div id="tarefas">
+          <div className="div-tarefa">
+            <div className="tarefas">
+              <div>
+                <div className="marcador">
+                  <input type="checkbox" defaultChecked={task.completed} />
+                </div>
+                <div className="data">
+                  <p>{task.date}</p>
+                </div>
+                <div className="titulo">
+                  <p>{task.name}</p>
+                </div>
+              </div>
+              <div>
+                <div className="button">
+                  <button type="button" onClick={editTask} id="btn-edit-task">
+                    <i className="bx bx-edit"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
 
-<div id="main-tarefas">
-<h1 className="topico">Tarefas</h1>
-<div id="tarefas">
-  <div className="div-tarefa">
-
-    <div className="tarefas">
-      <div>
-        <div className="marcador"><input type="checkbox" defaultChecked={task.completed}/></div>
-        <div className="data">
-          <p>{task.date}</p>
-        </div>
-        <div className="titulo">
-          <p>{task.name}</p>
-        </div>
-      </div>
-      <div>
-        <div className="button"><button type="button" onClick={editTask} id="btn-edit-task"><i className='bx bx-edit'></i></button></div>
-      </div>
-    </div>
-
-    <div id="edit-tarefas" style={{display: "none"}}>
-    <form  onSubmit={submit}>
-      <div>
-        <div className="marcador">
-          <input
-            type="checkbox"
-            onChange={(e) => setCompleted(e.target.checked)}
-            defaultChecked={task.completed}
-          />
-        </div>
-        <div className="data">
-          <input
-            type="date"
-            onChange={(e) => setDate(e.target.value)}
-            defaultValue={defaultDate}
-          />
+            <div id="edit-tarefas" style={{ display: "none" }}>
+              <form onSubmit={submit}>
+                <div>
+                  <div className="marcador">
+                    <input
+                      type="checkbox"
+                      onChange={(e) => setCompleted(e.target.checked)}
+                      defaultChecked={task.completed}
+                    />
+                  </div>
+                  <div className="data">
+                    <input
+                      type="date"
+                      onChange={(e) => setDate(e.target.value)}
+                      defaultValue={defaultDate}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <input id="btn-atualizar" type="submit" value="Atualizar" />
+                </div>
+                <div>
+                  <input
+                    id="btn-deletar-task"
+                    onClick={deletaTask}
+                    type="button"
+                    value="Deletar"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        <input id="btn-atualizar" type="submit" value="Atualizar" />
-      </div>
-      <div>
-        <input id="btn-deletar-task" type="submit" value="Deletar" />
-      </div>
-    </form>
-    </div>
-
-  </div>
-</div>
-</div>
-</>);
+    </>
+  );
 }
